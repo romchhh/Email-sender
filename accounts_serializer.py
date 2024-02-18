9afe7ef2f0272e1d5ee52e6aee5116260ba5b3f3
kxtzzl <123520267+romchhh@users.noreply.github.com>
2024-02-19 00:40:16 +0200

    Update accounts_serializer.py

--- accounts_serializer.py
+++ accounts_serializer.py
@@ -1,15 +1,16 @@
 from typing import List
 
 class Account():
-    def __init__(self, mail, key) -> None:
+    def __init__(self, mail, key, banned = False) -> None:
         self.mail = mail
         self.key = key
+        self.banned = banned
 
     def __str__(self) -> str:
-        return f'{self.mail} {self.key}'
+        return f'{self.mail} {self.key} banned:{self.banned}'
 
     def __repr__(self) -> str:
-        return f'{self.mail} {self.key}'
+        return f'{self.mail} {self.key} banned:{self.banned}'
 
 def get_accounts(account_file) -> List[Account]:
     accounts = list()
@@ -20,17 +21,20 @@ def get_accounts(account_file) -> List[Account]:
             if len(parts) >= 2:
                 mail = parts[0].strip()
                 key = parts[1].strip()
-                accounts.append(Account(mail, key))
+                banned = False
+                if len(parts) >= 3 and parts[2].strip(): 
+                    banned = True
+                accounts.append(Account(mail, key, banned))
     return accounts
+    
 
-
-   
-def register_stmp():
+def became_banned():
     pass
+
 
 if __name__ == '__main__':
 
 
     accounts = get_accounts(account_file='accounts.txt')
     for account in accounts:
-        print(account.mail, account.key)
+        print(account)
